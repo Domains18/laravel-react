cbc0a6e2b39f499aeaa82b9f76bc3ab2f6370d30
Domains18 <gibsonsgibson88@gmail.com>
2023-03-07 02:06:00 +0300

    login validator

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,10 +7,11 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function signup(SignupRequest $request)
     {
         $data = $request->validated();
         /** @var \App\Models\User */
@@ -23,8 +24,19 @@ class AuthController extends Controller
         $token = $user->createToken('main')->plainTextToken;
         return response(compact('user', 'token'));
     }
-    public function signup(SignupRequest $request)
+    public function login(LoginRequest $request)
     {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Invalid login details'
+            ]);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+
     }
     public function logout(Request $request)
     {
