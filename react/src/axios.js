344edb892d44168581dab0367cca9f956f98db06
Domains18 <gibsonsgibson88@gmail.com>
2023-03-08 04:38:18 +0300

    debugging

--- react/src/axios.js
+++ react/src/axios.js
@@ -1,29 +1,27 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api`
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
-
-//interceptors
-axiosClient.interceptors.request.use(async (config) => {x
-    const token = localStorage.getItem('token');
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('ACCESS_TOKEN');
     config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
 
 axiosClient.interceptors.response.use((response) => {
     return response;
+
 }, (error) => {
-    if (error.response.status === 401) {
+    const { response } = error;
+    if (response && response.status === 401) {
         localStorage.removeItem('ACCESS_TOKEN');
-        window.location.href = '/login';
-    } else {
-        return Promise.reject(error);
-    }
-    // return Promise.reject(error);
+        // window.location.href = '/login';
+    } 
+    throw new Error(error);
 });
 
-// Path: react/src/axios.js
+
 
 export default axiosClient;
