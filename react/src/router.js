7083c6e8d55114e0934ea1bc1036ae8b12b43b4d
Domains18 <gibsonsgibson88@gmail.com>
2023-03-21 18:16:50 +0000

    Mar 21, 2023, 9:16 PM

--- react/src/router.js
+++ react/src/router.js
@@ -7,6 +7,7 @@ import NotFound from './views/NotFound';
 
 
 const router = createBrowserRouter([
+    
     { path: '/login', element: <Login /> },
     { path: '/signup', element: <Signup /> },
     { path: '/users', element: <Users /> },
