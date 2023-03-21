ed9ac80e5e0899723efb33ed8d022ee6479b2142
Domains18 <gibsonsgibson88@gmail.com>
2023-03-21 18:08:17 +0000

    Mar 21, 2023, 9:08 PM

--- react/src/router.js
+++ react/src/router.js
@@ -2,11 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from './views/Login';
 import Signup from './views/Signup';
 import Users from './views/Users';
-
+Not
 
 
 const router = createBrowserRouter([
     { path: '/login', element: <Login /> },
+    {path: '*', element: <NotFound />},
 ]);
 
 
