b8fdae6bb4a8c18f758232baed16cd7690496b48
Domains18 <gibsonsgibson88@gmail.com>
2023-03-21 18:22:03 +0000

    Mar 21, 2023, 9:22 PM

--- react/src/router.js
+++ react/src/router.js
@@ -4,14 +4,23 @@ import Login from './views/Login';
 import Signup from './views/Signup';
 import Users from './views/Users';
 import NotFound from './views/NotFound';
-
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
 
 const router = createBrowserRouter([
-    
-    { path: '/login', element: <Login /> },
-    { path: '/signup', element: <Signup /> },
-    { path: '/users', element: <Users /> },
-    {path: '*', element: <NotFound />},
+    {
+        path: '/', element: <DefaultLayout />,
+        children: [
+            { path: '/users', element: <Users /> },
+
+        ]
+    },
+    {
+        path: '/', element: <GuestLayout />, children: [{ path: '/login', element: <Login /> },
+        { path: '/signup', element: <Signup /> },]
+    },
+
+    { path: '*', element: <NotFound /> },
 ]);
 
 
